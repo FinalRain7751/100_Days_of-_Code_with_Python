44b38da477bb37d4adfca982b91fb805e17e0be6
Souransu Sen <souransu2sen@gmail.com>
2023-05-22 02:39:33 +0530

    updated with opentrivia API integration

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
-from data import question_data
+from data import Question_bank
 from question_model import Question
 from quiz_brain import Quiz_brain
 
+while True:
+    try:
+        number_of_questions = int(
+            input("How many questions do you want?: "))  # Max 50
+        if number_of_questions > 50 or number_of_questions <= 0:
+            continue
+        break
+    except ValueError:
+        print("Enter a valid integer between 1 and 50.")
+
+
+question_data = Question_bank(10).question_data
 question_bank = []
-for question in question_data['results']:
+for question in question_data:
     text = question["question"]
     answer = question["correct_answer"]
     question_bank.append(Question(text, answer))
@@ -11,5 +23,5 @@ for question in question_data['results']:
 quiz = Quiz_brain(question_bank)
 
 while (quiz.still_has_questions()):
+    print()
     quiz.next_question()
-# def main():
