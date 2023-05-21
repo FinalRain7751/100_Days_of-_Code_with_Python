76c589fbde4fe487e9bbb83ee868811456f68ea7
Souransu Sen <souransu2sen@gmail.com>
2023-05-21 22:07:08 +0530

    coffee_machine_oop completed

--- coffeeMachine_oop.py
+++ coffeeMachine_oop.py
@@ -1,3 +1,36 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+import os
+
+
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
+
+def main():
+    while True:
+        order = (input(f"What would you like? ({menu.get_items()}) ")).lower()
+
+        if order == "off":
+            os.system("clear")
+            return
+
+        if order == "report":
+            coffee_maker.report()
+            money_machine.report()
+            continue
+
+        menu_item = menu.find_drink(order)
+        if not menu_item:
+            continue
+
+        if coffee_maker.is_resource_sufficient(menu_item):
+            if not money_machine.make_payment(menu_item.cost):
+                continue
+            coffee_maker.make_coffee(menu_item)
+
+
+if __name__ == "__main__":
+    main()
